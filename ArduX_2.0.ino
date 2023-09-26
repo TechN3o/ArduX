4c9fb77315f950c6d4565dabdb458871db0d2b85
Štěpán <100945112+TechN3o@users.noreply.github.com>
2023-09-26 18:31:24 +0200

    Add files via upload

--- ArduX_2.0.ino
+++ ArduX_2.0.ino
@@ -11,6 +11,7 @@ Copyright © 2023 Štěpán Otruba
 #include <Encoder.h>
 #include <Thread.h>
 #include <Timer.h>
+#include <Scheduler.h>
 //definitions
 #define lcdTopLeft 0, 0
 #define lcdTopRight 15, 0
@@ -40,7 +41,24 @@ Copyright © 2023 Štěpán Otruba
       3 - nothing
       4 - LCD: SDA
       5 - LCD: SCL    /*/
-
+//classes
+class functionInfoClass {
+public:
+  String name;
+  int listNumber;
+};
+void classSetup() {  // class objects
+  functionInfoClass sonicLCDClass;
+  sonicLCDClass.name = "sonic measuring";
+  functionInfoClass temperatureClass;
+  temperatureClass.name = "temperature";
+  functionInfoClass irClass;
+  irClass.name = "IR controll";
+  //class settings ♦️
+}
+String functionList[]{
+  "IR trans", "sonic measure"
+};
 
 //global variables:
 String debugInfoName;
@@ -53,17 +71,20 @@ String lastLcdReq;
 String lcdReq;
 Timer timer;
 int lcdRow;
-
-
+long encOldPos = -999;
+long encNewPos;
+Encoder myEnc(4, 5);
+int encPosition;
+int encoderReturn;
+int encLimitTo = 1;
 
 //debugInfo
 
-//USER SETTINGS ☢️
+//USER SETTINGS ☢️ ♦️
 int refreshRate = 2;  //seconds
-bool debugInfoOnOff = true;
-
-
-
+String debugInfoOnOff = "on";
+void debugInfo();
+void lcdPrint();
 void sonicLCD() {  //not my own code, source is unknown
   long duration, inches, cm;
   pinMode(pingPin, OUTPUT);
@@ -77,6 +98,7 @@ void sonicLCD() {  //not my own code, source is unknown
   inches = microsecondsToInches(duration);
   cm = microsecondsToCentimeters(duration);
   lcdPrint(String(cm));
+  debugInfo("SonicLcd", "work", "on", true);
   delay(300);
 }
 long microsecondsToInches(long microseconds) {
@@ -86,38 +108,51 @@ long microsecondsToInches(long microseconds) {
 long microsecondsToCentimeters(long microseconds) {
   return microseconds / 29 / 2;
 }
+template<typename T, size_t N>
+int getArraySize(T (&array)[N]) {
+  int arrayLength = sizeof(array) / sizeof(array[0]);
+  Serial.println(arrayLength);
+  return arrayLength;
+}
+void menuSetup(int menuArrayLenght) {
+  encLimitTo = menuArrayLenght;
+}
 
 
-// example of use: debugInfo("lcd motor", "calculate mass of sun", "idk" (depends on if I turned dbgInfOnOff on or off - else "true" of "false"), MyVar == 10)
+
+// example of use: debugInfo("lcd motor", "calculate mass of sun", "idk" **depends on if I turned dbgInfOnOff to on or off - else "true" or "false"**, MyVar == 10)
 bool debugInfo(String debugInfoName, String debugInfoDescription, String debugInfoSingleOnOff = "idk", bool debugInfoExpect = false) {
-  if (debugInfoSingleOnOff == "idk" or "on") {
-    if (debugInfoOnOff or debugInfoSingleOnOff == "true") {
+  if (debugInfoSingleOnOff == "idk" or debugInfoSingleOnOff == "on") {
+    if (debugInfoSingleOnOff == "on" || debugInfoOnOff == "on") {
       Serial.print(debugInfoName);
       if (debugInfoExpect) {
         Serial.print(" did ");
         Serial.print(debugInfoDescription);
-        Serial.print(", as expected  ");
+        Serial.print(", as expected");
         Serial.println();
         return true;
       } else {
-        Serial.print(" didnt ");
+        Serial.print(" didn't ");
         Serial.print(debugInfoDescription);
-        Serial.print(" beacuse has failed/unspecified expecting  ");
+        Serial.print(" because it has failed/unspecified, expecting");
         Serial.println();
         return false;
       }
     }
   } else {
-    return false;  //no info
+    return false;  // No info
   }
 }
-int lcdPrint(String lcdReq) {
-  if (lcdReq != lastLcdReq) {
+
+void lcdPrint(String lcdReq = "lcdPrint.no.task") {
+  if (lcdReq == lastLcdReq || lcdReq == "0x0") {
+    debugInfo("lcdPrint", "nothing (no requests)", "on", true);
+  } else if (lcdReq != lastLcdReq) {
     lcd.clear();
     lcd.print(lcdReq);
     lastLcdReq = lcdReq;
     lcd.setCursor(lcdTopLeft);
-    debugInfo("lcdPrint", "refresh for a new text", "idk", lastLcdReq == lcdReq);
+    debugInfo("lcdPrint", "refresh for a new text", "off", lastLcdReq == lcdReq);
   }
   if (timer.read() >= refreshRate * 1000) {
     lcd.clear();
@@ -125,12 +160,46 @@ int lcdPrint(String lcdReq) {
     timer.stop();
     timer.start();
   }
-  if (lcdReq == lastLcdReq) {
-    debugInfo("lcdPrint", "nothing (no requests)", "idk");
-  }
 }
+
+
+
+void encoderPosition() {  //before calling function MUST call function (thats bullshit :D)
+
+  // READING from encoder
+  if (encNewPos != encOldPos) {
+    encOldPos = encNewPos;
+    encPosition = encNewPos;  // returning "encPosition" that WILL be limited (down bellow) (encOldPos and encNewPos are just ONLY for making sure that something has to be done if encoder.read() CHANGE to smthg different )
+    debugInfo("encoder", "change position", "idk", encOldPos == encNewPos);
+  }
+  debugInfo("encoder", String(myEnc.read()), "off", true);
+
+
+  // LIMITING encoder position (encPosition) by variable "encLimitTo"
+  if (encPosition < 0) {
+    myEnc.write(0);
+    encPosition = 0;
+  }
+  if (encPosition >= encLimitTo) {
+    myEnc.write(encLimitTo);
+    encPosition = encLimitTo;
+  }
+  debugInfo("EncoderPosition", String(encPosition), "on", true);
+  encoderReturn = encPosition;
+}
+
+
+
 void setup() {
   Serial.begin(9600);
+  while (!Serial)
+    ;
+  //scheduler:
+  // Scheduler.startLoop(encoderPosition);
+  //class setup
+  void classSetup();
+
+
   lcd.init();
   lcd.backlight();
   lcd.clear();
@@ -140,6 +209,6 @@ void setup() {
 }
 
 void loop() {
-  sonicLCD();
-  debugInfo("Serial test", "work", "on", Serial.available());
+  // menuSetup(getArraySize(functionList));
+   lcdPrint();
 }
